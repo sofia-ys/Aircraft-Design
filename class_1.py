f92e471346b2d1138487be06fc58982a476ec634
Jambuska <bence.gecseg@gmail.com>
2024-09-04 16:17:18 +0200

    linear regression with data arrays

--- class_1.py
+++ class_1.py
@@ -1,63 +1,37 @@
-# class 1 estimations
-# comment 2
-# comment 3
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
 
+#Data
+max_payload = np.array([25970, 41050, 18711, 23133, 18325, 22000, 62250, 23000, 16150, 40000, 33355])
+MTOM = np.array([115680, 228000, 70896, 87997, 141521, 186880, 273310, 165000, 61500, 250000, 211374])
+max_range = np.array([7222, 13530, 6300, 7408, 8112, 11305, 12270, 8000, 4815, 10000, 6862])
+OEM = np.array([58390, 119950, 37080, 45450, 66670, 90770, 132800, 74000, 33300, 120400, 111795])
 
+def linear_regression(p, y) :
+    #convert p in appropriate form (sklearn = weird)
+    X = p.reshape(-1, 1)
+    # Train the model
+    model = LinearRegression()
+    model.fit(X, y)
 
-import numpy as nmp
-import matplotlib.pyplot as mtplt
+    # Make predictions
+    y_pred = model.predict(X)
 
-max_payload = nmp.array([25970, 41050, 18711, 23133, 18325, 22000, 62250, 23000, 16150, 40000, 33355])
-MTOM = nmp.array([115680, 228000, 70896, 87997, 141521, 186880, 273310, 165000, 61500, 250000, 211374])
-max_range = nmp.array([7222, 13530, 6300, 7408, 8112, 11305, 12270, 8000, 4815, 10000, 6862])
-OEM = nmp.array([58390, 119950, 37080, 45450, 66670, 90770, 132800, 74000, 33300, 120400, 111795])
+    # Plotting the regression line
+    plt.scatter(X, y, color='blue', label='Actual Data')
+    plt.plot(X, y_pred, color='red', label='Regression Line')
+    plt.xlabel('just type the name manually (x axis)')
+    plt.ylabel('just type the name manually (y axis)')
+    plt.legend()
 
-def estimate_coeff(p, q):
-    # Here, we will estimate the total number of points or observation
-    n1 = nmp.size(p)
-    # Now, we will calculate the mean of a and b vector
-    m_p = nmp.mean(p)
-    m_q = nmp.mean(q)
+    # Display the plot
+    plt.show()
 
-    # here, we will calculate the cross deviation and deviation about a
-    SS_pq = nmp.sum(q * p) - n1 * m_q * m_p
-    SS_pp = nmp.sum(p * p) - n1 * m_p * m_p
+    # Get the regression coefficients
+    b_1 = model.coef_[0]  # Slope (coefficient of X)
+    b_0 = model.intercept_ # Intercept
 
-    # here, we will calculate the regression coefficients
-    b_1 = SS_pq / SS_pp
-    b_0 = m_q - b_1 * m_p
+    print(f"Estimated coefficients:\nb_0 (intercept) = {b_0}\nb_1 (slope) = {b_1}")
 
-    return (b_0, b_1)
-
-
-def plot_regression_line(p, q, b):
-    # Now, we will plot the actual points or observation as scatter plot
-    mtplt.scatter(p, q, color="m",
-                  marker="o", s=30)
-
-    # here, we will calculate the predicted response vector
-    q_pred = b[0] + b[1] * p
-
-    # here, we will plot the regression line
-    mtplt.plot(p, q_pred, color="g")
-
-    # here, we will put the labels
-    mtplt.xlabel('p')
-    mtplt.ylabel('q')
-
-    # here, we will define the function to show plot
-    mtplt.show()
-
-
-def main(p, q):
-    # now, we will estimate the coefficients
-    b = estimate_coeff(p, q)
-    print("Estimated coefficients are :\nb_0 = {} \
-    \nb_1 = {}".format(b[0], b[1]))
-
-    # Now, we will plot the regression line
-    plot_regression_line(p, q, b)
-
-    if __name__ == "__main__":
-        main()
-main(OEM, max_payload)
+linear_regression(max_payload, MTOM)
