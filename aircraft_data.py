a4376f80085d31107e06c53d1a2106549cc8c884
Mishok-code <Mikfedotov2004@gmail.com>
2024-09-05 23:09:39 +0200

    -

---
+++ aircraft_data.py
@@ -0,0 +1,9 @@
+# Data file for aircraft parameters and constants
+
+payload_max = 18960 # [kg]
+M = 0.85 # cruise mach number at 31000 [ft]
+dist_take_off = 10000 # [ft]
+dist_landing = 6500 # [ft]
+range_harmonic = 9545 # [km] at 18960 [kg]
+range_MTOW_full_fuel = 11716 # [km] at a payload of 8531 [kg]
+range_ferry = 12697 # [km]
