a73ab64ce9573e159bb40c82867980944b34998c
Mishok-code <Mikfedotov2004@gmail.com>
2024-09-07 23:57:10 +0200

    -

--- aircraft_data.py
+++ aircraft_data.py
@@ -1,5 +1,11 @@
 # Data file for aircraft parameters and constants
 
+g = 9.80665
+R = 287
+T_sl = 288 # [K] temp at sea level isa
+P_sl = 101000 # [Pa] pressure at sea level isa
+a = -0.0065 # [K/m] temperature lapse rate with altitude
+
 payload_max = 18960 # [kg]
 M = 0.85 # cruise mach number at 31000 [ft]
 dist_take_off = 10000 # [ft]
@@ -8,9 +14,17 @@ range_harmonic = 9545 # [km] at 18960 [kg]
 range_MTOW_full_fuel = 11716 # [km] at a payload of 8531 [kg]
 range_ferry = 12697 # [km]
 speed_approach = 74.6 # [m/s]
+altitude_landing = 1600 # [m] landing altitude suggested in the book, maybe should be changed
+T_delta = 15 # [K] hot day conditions
+
 cl_cruise = 1.5
 cl_landing = 2.5
 cl_take_off = 2
-m_fraq_min_speed = 0.85
+
+m_fraq_landing = 0.85
+
 c_lfl = 0.45 # [s^2/m] landing field length coefficient suggested in adsee book p.133
-l_lf = c_lfl * speed_approach ** 2 # landing field length
+l_fl = c_lfl * speed_approach ** 2 # landing field length
+P_landing = P_sl * (1 + (a * altitude_landing) / T_sl) ** (-g / (a * R))
+T_landing = T_sl + T_delta + a * altitude_landing
+rho_landing = P_landing / (R * T_landing)
--- matching_diagram.py
+++ matching_diagram.py
@@ -7,12 +7,16 @@ import aircraft_data as ad
 wing_loading = np.linspace(0, 9000, num=91)
 
 # W/S [N/m^2] Calculating the maximum wing loading for the minimum speed requirement
-min_speed = (1 / ad.m_fraq_min_speed * (1.225 / 2) * (ad.speed_approach / 1.23) ** 2 * ad.cl_landing)
+min_speed = (1 / ad.m_fraq_landing * (1.225 / 2) * (ad.speed_approach / 1.23) ** 2 * ad.cl_landing)
+
+# W/S [N/m^2] Calculating the maximum wing loading for the landing field length requirement
+landing_field_length = (1 / ad.m_fraq_landing) * (ad.l_fl / ad.c_lfl) * ((ad.rho_landing * ad.cl_landing) / 2)
 
 # Plot size
 plt.figure(figsize=(12, 8))
 
 plt.axvline(x = min_speed, color = 'b', label = 'Minimum speed')
+plt.axvline(x = landing_field_length, color = 'orange' , label = 'Landing field length')
 
 plt.legend(loc = 'upper left')
 
