17db0f53b5eed012f712967c7f47e90024edf05c
Mishok-code <Mikfedotov2004@gmail.com>
2024-09-09 17:14:48 +0200

    -

--- matching_diagram.py
+++ matching_diagram.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import aircraft_data as ad
 import cruise_speed
+import climb_gradient
 
 # W/S [N/m^2] Generating the wing loading list 100 to 9000 with increment of 100
 wing_loading = np.linspace(100, 9000, num=90)
@@ -15,12 +16,15 @@ landing_field_length = (1 / ad.m_fraq_landing) * (ad.l_fl / ad.c_lfl) * ((ad.rho
 
 cruise_speed = cruise_speed.cruise_thrust_to_weight(wing_loading)
 
+climb_gradient_119 = climb_gradient.calcGradient119(wing_loading)
+
 # Plot size
 plt.figure(figsize=(12, 8))
 
 plt.axvline(x = min_speed, color = 'b', label = 'Minimum speed')
 plt.axvline(x = landing_field_length, color = 'orange' , label = 'Landing field length')
 plt.plot(wing_loading, cruise_speed, color = 'purple', label = 'Cruise speed')
+plt.plot(wing_loading, climb_gradient_119)
 
 plt.legend(loc = 'upper left')
 
