aa0b2cf3007d5abbe422a4de2c80751d783c5412
Mishok-code <Mikfedotov2004@gmail.com>
2024-09-09 16:40:58 +0200

    -

--- matching_diagram.py
+++ matching_diagram.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import aircraft_data as ad
+import cruise_speed
 
 # W/S [N/m^2] Generating the wing loading list 100 to 9000 with increment of 100
 wing_loading = np.linspace(100, 9000, num=90)
@@ -12,11 +13,14 @@ min_speed = (1 / ad.m_fraq_landing * (1.225 / 2) * (ad.speed_stall_landing_field
 # W/S [N/m^2] Calculating the maximum wing loading for the landing field length requirement
 landing_field_length = (1 / ad.m_fraq_landing) * (ad.l_fl / ad.c_lfl) * ((ad.rho_landing * ad.cl_landing) / 2)
 
+cruise_speed = cruise_speed.cruise_thrust_to_weight(wing_loading)
+
 # Plot size
 plt.figure(figsize=(12, 8))
 
 plt.axvline(x = min_speed, color = 'b', label = 'Minimum speed')
 plt.axvline(x = landing_field_length, color = 'orange' , label = 'Landing field length')
+plt.plot(wing_loading, cruise_speed, color = 'purple', label = 'Cruise speed')
 
 plt.legend(loc = 'upper left')
 
