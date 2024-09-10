f1c328d784135f72af188e251c06bf16fbda43e3
sofia yanes sanchez <85056219+sofia-ys@users.noreply.github.com>
2024-09-10 23:51:16 +0200

    class I layout for work to do

--- class-i estimations.py
+++ class-i estimations.py
@@ -8,7 +8,7 @@ ra = pd.read_excel("Reference_aircraft.xlsx", sheet_name="Sheet 1")
 MTOM = ra.iloc[:, 3]
 OEM = ra.iloc[:, 4]
 
-plt.scatter(MTOM, OEM, label="Data points")  # scatter plot
+plt.scatter(MTOM, OEM, color="black", label="Data points")  # scatter plot
 
 # finding line of best fit
 coefficients = np.polyfit(MTOM, OEM, 1)  # fits a polynomial of first degree to the relationship between MTOM and OEM
@@ -21,7 +21,7 @@ res = np.sum((OEM - linearReg)**2)
 tot = np.sum((OEM - np.mean(OEM))**2)
 rSqr = 1 - (res/tot)
 
-plt.plot(MTOM, linearReg, color="green", label=f"Linear regression (R² = {rSqr:.2f})")  # plotting line of best fit
+plt.plot(MTOM, linearReg, color="#8ace00", label=f"Linear regression (R² = {rSqr:.2f})")  # plotting line of best fit
 
 plt.xlabel("Maximum Take-Off Mass (MTOM) [kg]")
 plt.ylabel("Operating Empty Mass (OEM) [kg]")
@@ -32,12 +32,18 @@ plt.show()
 
 ''' 2. estimate drag polar and specific fuel consumption'''
 
-
+# drag polar slides 29 - 36
 
 
 ''' 3. estimate/calculate fuel fractions'''
 
+# fuel weight estimation slide 16 - 28
+
+# fuel estimation slides 37 - 44
 
 
+'''4. compute MTOW, OEW and W_fuel'''
 
-'''4. computer MTOW, OEW and W_fuel'''
+# calculate empty weight from W_TO slide 15
+
+# examples on procedure slides 45 - 54
