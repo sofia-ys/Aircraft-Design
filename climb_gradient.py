0fd21ad8eff194145bded9ef2975c0d9a6cdd626
Mishok-code <Mikfedotov2004@gmail.com>
2024-09-11 15:28:10 +0200

    -

--- climb_gradient.py
+++ climb_gradient.py
@@ -3,7 +3,7 @@ import aircraft_data as ad
 
 
 # create a function to calculate all flight gradient requirements
-def calcClimbGradientGeneral(wing_loading, cl_req, cd_0, oswald, c_v, beta, thrust_fraq):
+def calcClimbGradientGeneral(wing_loading, cl_req, cd_0, oswald, c_v, beta, thrust_fraq, T, P, rho):
     V = []
     M = []
     theta_t = []
@@ -12,16 +12,16 @@ def calcClimbGradientGeneral(wing_loading, cl_req, cd_0, oswald, c_v, beta, thru
     B = ad.bypass
     climb_gradient = []
     for i in wing_loading:
-        V.append(ma.sqrt((2 * i) / (1.225 * cl_req / 1.1)))
+        V.append(ma.sqrt((2 * i) / (rho * cl_req / 1.1)))
 
     for i in V:
-        M.append(i / (ma.sqrt(1.4 * 287 * 288.15)))  # using sea level values, since all the flight gradient requirements use it
+        M.append(i / (ma.sqrt(1.4 * 287 * T)))
 
     for i in M:
-        theta_t.append((ad.T_take_off * (1 + 0.2 * i ** 2)) / ad.T_sl)
+        theta_t.append((T * (1 + 0.2 * i ** 2)) / ad.T_sl)
 
     for i in M:
-        delta_t.append((ad.P_take_off * pow((1 + 0.2 * pow(i, 2)), (1.4 / 0.4))) / ad.P_sl)
+        delta_t.append((P * pow((1 + 0.2 * pow(i, 2)), (1.4 / 0.4))) / ad.P_sl)
 
     for i in range(90):
         alpha_t.append(delta_t[i] * (1 - (0.43 + 0.014 * B) * ma.sqrt(M[i])))
@@ -41,7 +41,7 @@ def calcGradient119(wing_loading):
     thrust_fraq = 1
     cd0_adj = ad.c_d0 + 35 * 0.0013 + 0.0175
     osw_adj = ad.oswald + 0.0026 * 35
-    T_W = calcClimbGradientGeneral(wing_loading, ad.cl_landing, cd0_adj, osw_adj, c_v, beta, thrust_fraq)  # np.linspace(100, 9000, num=90)
+    T_W = calcClimbGradientGeneral(wing_loading, ad.cl_landing, cd0_adj, osw_adj, c_v, beta, thrust_fraq, ad.T_landing, ad.P_landing, ad.rho_landing)  # np.linspace(100, 9000, num=90)
     return T_W
 
 
@@ -52,7 +52,7 @@ def calcGradient121a(wing_loading):
     thrust_fraq = ad.Ne / (ad.Ne - 1)
     cd0_adj = ad.c_d0 + 15 * 0.0013 + 0.0175
     osw_adj = ad.oswald + 0.0026 * 15
-    T_W = calcClimbGradientGeneral(wing_loading, ad.cl_take_off, cd0_adj, osw_adj, c_v, beta, thrust_fraq)
+    T_W = calcClimbGradientGeneral(wing_loading, ad.cl_take_off, cd0_adj, osw_adj, c_v, beta, thrust_fraq, ad.T_take_off, ad.P_take_off, ad.rho_take_off)
     return T_W
 
 
@@ -62,7 +62,7 @@ def calcGradient121b(wing_loading):
     thrust_fraq = ad.Ne / (ad.Ne - 1)
     cd0_adj = ad.c_d0 + 15 * 0.0013
     osw_adj = ad.oswald + 0.0026 * 15
-    T_W = calcClimbGradientGeneral(wing_loading, ad.cl_take_off, cd0_adj, osw_adj, c_v, beta, thrust_fraq)
+    T_W = calcClimbGradientGeneral(wing_loading, ad.cl_take_off, cd0_adj, osw_adj, c_v, beta, thrust_fraq, ad.T_take_off, ad.P_take_off, ad.rho_take_off)
     return T_W
 
 
@@ -72,7 +72,7 @@ def calcGradient121c(wing_loading):
     thrust_fraq = ad.Ne / (ad.Ne - 1)
     cd0_adj = ad.c_d0
     osw_adj = ad.oswald
-    T_W = calcClimbGradientGeneral(wing_loading, ad.cl_cruise, cd0_adj, osw_adj, c_v, beta, thrust_fraq)
+    T_W = calcClimbGradientGeneral(wing_loading, ad.cl_cruise, cd0_adj, osw_adj, c_v, beta, thrust_fraq, ad.T_take_off, ad.P_take_off, ad.rho_take_off)
     return T_W
 
 
@@ -82,5 +82,5 @@ def calcGradient121d(wing_loading):
     thrust_fraq = ad.Ne / (ad.Ne - 1)
     cd0_adj = ad.c_d0 + 35 * 0.0013
     osw_adj = ad.oswald + 0.0026 * 35
-    T_W = calcClimbGradientGeneral(wing_loading, ad.cl_landing, cd0_adj, osw_adj, c_v, beta, thrust_fraq)
+    T_W = calcClimbGradientGeneral(wing_loading, ad.cl_landing, cd0_adj, osw_adj, c_v, beta, thrust_fraq, ad.T_landing, ad.P_landing, ad.rho_landing)
     return T_W
