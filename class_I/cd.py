628c4a34637a1173b1dea42a44f08c7f82ed7d9d
Goga64 <goga.myak@gmail.com>
2024-09-11 14:32:33 +0200

    cl_cruse + cd

--- class_I/cd.py
+++ class_I/cd.py
@@ -1,11 +1,12 @@
 import sys
 import os
-print(os.path.abspath('Aircraft-Design'))
 
-# Add the directory to sys.path
-sys.path.append(os.path.abspath('AIRCRAFT-DESIGN'))
+# Add the parent directory to sys.path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-# Now you can import the variable from module.py
-#from aircraft_data import *
+# Now import the variable from aircraft_data.py
+from aircraft_data import *
 
-##print(oswald)
+c_friction = 0.0029 #From fig 6.3
+cd0 = S_wet_to_S *c_friction
+cdi = 0
