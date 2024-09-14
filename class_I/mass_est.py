cb14d6110f6bad5324f402379f71e6e730677a7e
AdamJaneczech <anndyjan@seznam.cz>
2024-09-14 16:07:16 +0200

    mass_est_comments

--- class_I/mass_est.py
+++ class_I/mass_est.py
@@ -3,13 +3,8 @@ import numpy as np
 import math
 import cd
 import payloadrange
-
+# Combined engine efficiencies
 n_eng_times_n_p = 0.4275727
-# Calculate the C_D
-
-C_D0 = cd.cd0
-C_L = cd.cl_cruise
-C_D = C_D0 + pow(C_L, 2) / (math.pi * cd.oswald * cd.AR)
 # Calculate the R_eq
 R_lost = (1/0.7) * (cd.l_d_cruise) * (cd.altitude_cruise + (pow(cd.velocity_cruise, 2) / (2 * 9.81)))
 R_eq = (payloadrange.designRange + R_lost / 1000) * (1 + 0.05) + 1.2 * 250 + (30 * 60) * (cd.velocity_cruise / 1000)
@@ -21,4 +16,5 @@ M_mto = (1556.2 + cd.payload_max)/(1-0.4874-m_ff)
 print('R_lost' + str(R_lost))
 print('R_eq (equivalent range): ' + str(R_eq))
 print('Fuel mass fraction: ' + str(m_ff))
+print('\n')
 print('Maximum takeoff mass: ' + str(M_mto))
