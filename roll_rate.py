0f107699197db93f60bac0f5800739deef9baf15
sofia yanes sanchez <85056219+sofia-ys@users.noreply.github.com>
2024-09-23 13:42:28 +0200

    roll rate

--- roll_rate.py
+++ roll_rate.py
@@ -21,23 +21,36 @@ def findCY(xPos):
 intClDeltTab = np.zeros((int((b/2)/delX), int((b/2)/delX)))  # making an array for this integral
 for i1idx, i1 in enumerate(np.arange(0, b/2, delX)):
     for i2idx, i2 in enumerate(np.arange(i1, b/2, delX)):
-        intClDeltTab[i1idx, i2idx] = findCY(i2) * 0.5 * (i2)**2 - findCY(i1) * 0.5 * (i1)**2
+        intClDeltTab[i1idx, i2idx] = findCY(i2) * 0.5 * (i2)**2 - findCY(i1) * 0.5 * (i1)**2                                                                                                                                                                                                                 
 
 intClPTab = np.zeros((int((b/2)/delX), int((b/2)/delX)))  # making an array for this integral
 for j1idx, j1 in enumerate(np.arange(0, b/2, delX)):
     for j2idx, j2 in enumerate(np.arange(i1, b/2, delX)):
         intClDeltTab[j1idx, j2idx] = findCY(j2) * (1/3) * (j2)**3 - findCY(j1) * (1/3) * (j1)**3  # at the index according to the iteration number, insert evaluated integral
 
-# Ptab = []
-# for k in range(len(intClDeltTab)):
-#     clDelta = (2 * clAlpha * tau)/(sRef * b) * intClDeltTab[k]
-#     clP = (-4 * (clAlpha + cd0))/(sRef * b**2) * intClPTab[k]
-#     P = -clDelta/clP * deltaA * (2 * velocity)/b
-#     Ptab.append(P)
+Ptab = np.zeros((int((b/2)/delX), int((b/2)/delX)))  # making an array for this integral
+rP, cP = Ptab.shape
+for k in range(rP):
+    for m in range(cP):
+        clDelta = (2 * clAlpha * tau)/(sRef * b) * intClDeltTab[k, m]
+        clP = (-4 * (clAlpha + cd0))/(sRef * b**2) * intClPTab[k, m]
 
-# difTab = []
-# for m in range(len(Ptab)):
-#     dif = Ptab[m] - Pideal
-#     difTab.append(dif)
+        if clP == 0:  # making sure no divide by zero
+            continue
+        else:
+            P = -clDelta/clP * deltaA * (2 * velocity)/b
+            Ptab[k, m] = P
 
-# bInd = difTab.index(min(difTab))
+difTab = np.zeros((int((b/2)/delX), int((b/2)/delX)))  # making an array for this integral
+rD, cD = difTab.shape
+for n in range(rD):
+    for p in range(cD):
+        dif = Ptab[n, p] - Pideal
+        difTab[n, p] = dif
+
+idx1, idx2 = np.where(difTab == np.min(difTab))
+
+b1 = idx1 * delX
+b2 = idx1 * delX + idx2 * delX
+
+print(b1, b2)
