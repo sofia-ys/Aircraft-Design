fcbf4c8c302291756015c343334bed34810def7b
sofia yanes sanchez <85056219+sofia-ys@users.noreply.github.com>
2024-09-23 03:45:05 +0200

    mac diagram complete

--- mac.py
+++ mac.py
@@ -11,8 +11,8 @@ cTip = 2.07  # tip chord m
 sweep = 0.537  # wing sweep rad
 
 def graph(line, colour):
-    for x, y in line:
-        plt.plot(x, y, color=colour)
+    x, y = line
+    plt.plot(x, y, color=colour)
 
 def lineEq(x1, y1, x2, y2):
     m = (y2 - y1) / (x2 - x1)
@@ -20,14 +20,14 @@ def lineEq(x1, y1, x2, y2):
     return m, c
 
 # graph segments
-root = [([0, 0], [0, -cRoot])]
-rootEx = [([0, 0], [-cRoot, (-cRoot - cTip)])]
-quarterC = [([0, b/2], [-0.25*cRoot, (-0.25*cRoot - (b/2)*math.tan(sweep))])]
-tip = [([b/2, b/2], [(-0.25*cRoot - (b/2)*math.tan(sweep) + 0.25*cTip), (-0.25*cRoot - (b/2)*math.tan(sweep) - 0.75*cTip)])]
-tipEx = [([b/2, b/2], [(-0.25*cRoot - (b/2)*math.tan(sweep) + 0.25*cTip), (-0.25*cRoot - (b/2)*math.tan(sweep) + 0.25*cTip + cRoot)])]
-leadingE = [([0, b/2], [0, (-0.25*cRoot - (b/2)*math.tan(sweep) + 0.25*cTip)])]
-trailingE = [([0, b/2], [-cRoot, (-0.25*cRoot - (b/2)*math.tan(sweep) - 0.75*cTip)])]
-diagonal = [([0, b/2], [(-cRoot - cTip), (-0.25*cRoot - (b/2)*math.tan(sweep) + 0.25*cTip + cRoot)])]
+root = ([0, 0], [0, -cRoot])
+rootEx = ([0, 0], [-cRoot, (-cRoot - cTip)])
+quarterC = ([0, b/2], [-0.25*cRoot, (-0.25*cRoot - (b/2)*math.tan(sweep))])
+tip = ([b/2, b/2], [(-0.25*cRoot - (b/2)*math.tan(sweep) + 0.25*cTip), (-0.25*cRoot - (b/2)*math.tan(sweep) - 0.75*cTip)])
+tipEx = ([b/2, b/2], [(-0.25*cRoot - (b/2)*math.tan(sweep) + 0.25*cTip), (-0.25*cRoot - (b/2)*math.tan(sweep) + 0.25*cTip + cRoot)])
+leadingE = ([0, b/2], [0, (-0.25*cRoot - (b/2)*math.tan(sweep) + 0.25*cTip)])
+trailingE = ([0, b/2], [-cRoot, (-0.25*cRoot - (b/2)*math.tan(sweep) - 0.75*cTip)])
+diagonal = ([0, b/2], [(-cRoot - cTip), (-0.25*cRoot - (b/2)*math.tan(sweep) + 0.25*cTip + cRoot)])
 
 # graphing wing planform
 graph(root, "black")
@@ -39,14 +39,14 @@ graph(leadingE, "black")
 graph(trailingE, "black")
 graph(diagonal, "#8ace00")
 
-# find intersection
-# mLeading, cLeading = lineEq(leadingE[0][0], leadingE[1][0], leadingE[0][1], leadingE[1][1])
-# mDiagonal, cDiagonal = lineEq(diagonal[0][0], diagonal[1][0], diagonal[0][1], diagonal[1][1])
-# xInter = (cDiagonal - cLeading) / (mLeading - mDiagonal)
-# yInter = mLeading * xInter + cLeading
+# finding MAC
+mLeading, cLeading = lineEq(leadingE[0][0], leadingE[1][0], leadingE[0][1], leadingE[1][1])
+mDiagonal, cDiagonal = lineEq(diagonal[0][0], diagonal[1][0], diagonal[0][1], diagonal[1][1])
+xInter = (cDiagonal - cLeading) / (mLeading - mDiagonal)
+yInter = mLeading * xInter + cLeading
+MAC = ([xInter, xInter], [yInter, (((-0.25*cRoot - (b/2)*math.tan(sweep) - 0.75*cTip) + cRoot)/(b/2) * xInter - cRoot)])
 
-# plt.plot(xInter, yInter, 'ro', label="Intersection Point")
+graph(MAC, "#8ace00")
 
-# print(f"Intersection point: ({xInter:.2f}, {yInter:.2f})")
-# plt.legend()
-plt.plot()
+print(f"MAC: ({xInter:.2f}, {yInter:.2f})")
+plt.show()
