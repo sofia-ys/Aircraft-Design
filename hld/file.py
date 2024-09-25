344659495065e3ed0be605f8c748b1dc4392aaf2
AdamJaneczech <anndyjan@seznam.cz>
2024-09-25 15:21:43 +0200

    hld_update

---
+++ hld/file.py
@@ -0,0 +1,26 @@
+import math
+from matplotlib import pyplot
+import numpy as np
+
+deltaC_L = 0.52 # how much do we need to increase the C_L with HLDs
+
+LE_sweep = 2
+
+fowlerDeflection = math.radians(40)
+slottedDeflection = math.radians(40)
+
+deltac_cf_fowler = 0.62
+deltac_cf_slotted = 0.31
+
+cf_over_c = 0.1 #this value will be iterated - range from 0.1 to 0.4
+
+#Fowler
+delta_c_c_fowler = cf_over_c * deltac_cf_fowler
+cdash_c_fowler = 1 + delta_c_c_fowler
+#Slotted
+delta_c_c_slotted = cf_over_c * deltac_cf_slotted
+cdash_c_slotted = 1 + delta_c_c_slotted
+
+delta_Clmax_fowler = 1.3 * cdash_c_fowler
+delta_Clmax_slotted = 1.3
+
