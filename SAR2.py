5fa90ba860a4f84882a8e2556b52158a5b64f6df
Adria-gj <adriagasconj@gmail.com>
2024-10-06 13:26:33 +0200

    Update SAR2.py

--- SAR2.py
+++ SAR2.py
@@ -1,10 +1,8 @@
 import math
 S = 173.77  # wing area m^2
 AR = 10# aspect ratio
-b = math.sqrt(AR*S)  # wing span m
-taper = 0.292  # taper ratio
-cRoot = (2*S)/((taper+1)*b) # root chord m
-cTip = taper*cRoot  # tip chord m
+b = math.sqrt(AR*S)  # wing span m  # taper ratio
+
 sweep_quarter = 0.537  # wing sweep rad
 C_D0 = 0.01589727272
 CL= 0.454
@@ -14,10 +12,10 @@ C_T= 22*(B**-0.19)*(1*(10**-6))
 
 sweep_quarter = 0.537
 sweep_LE = 0.5871613666551182
-Cr = 2 * math.sqrt(AR * S) * (math.tan(sweep_LE) - math.tan(sweep_quarter)) + (S / (math.sqrt(AR * S)))
+Cr =  math.sqrt(AR * S) * (math.tan(sweep_LE) - math.tan(sweep_quarter)) + (S / (math.sqrt(AR * S)))
 Taper_ratio = ((2 * S) / (Cr * math.sqrt(AR * S))) - 1
 Ctip = Cr * Taper_ratio
-Lambda_half = math.atan(math.tan(sweep_LE) - (0.5 * (2 * cRoot / b) * (1 - taper)))
+Lambda_half = math.atan(math.tan(sweep_LE) - (0.5 * (2 * Cr / b) * (1 - Taper_ratio)))
 #Lambda_05c = 0.48689
 e = 2 / (2 - AR + math.sqrt(4 + AR**2 * (1 + math.tan(Lambda_half)**2)))
 L= 117604*0.9*9.80665
@@ -26,5 +24,8 @@ SAR= v_cruise/(D_cruise*C_T)
 print(D_cruise)
 print(C_T)
 print(SAR)
+print(Cr)
+print(Taper_ratio)
+print(Ctip)
 
-print(cTip)
+
