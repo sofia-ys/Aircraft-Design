2df7cee3f4a732a90edf20d4f25fcfa993dbc0f3
Goga64 <goga.myak@gmail.com>
2024-10-09 14:17:26 +0200

    creating clas ii

---
+++ class_ii/Drag.py
@@ -0,0 +1,25 @@
+import math as m
+
+
+
+
+
+Re = 
+
+
+
+
+
+MAC = C_MGC = (2 / 3) * C_r * ((1 + taper_ratio + taper_ratio**2) / (1 + taper_ratio))
+Fus_lam_frac = 0.05
+Wing_lam_frac = 10
+
+C_f_lam = (1.382)/(m.sqrt(Re))
+
+
+
+
+
+
+
+CD_0 = (1/S_ref)*(C_f_c * FF_c * IF_c * S_wet_c) + C_D_misc
