6f60dc79853e4d937dce20b02b32af12b845f575
sofia yanes sanchez <85056219+sofia-ys@users.noreply.github.com>
2024-11-13 16:04:26 +0100

    add

--- class_I/cd.py
+++ class_I/cd.py
@@ -8,7 +8,7 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 # Now import the variable from aircraft_data.py
 from aircraft_data import *
 c_friction = 0.0029 #From fig 6.3
-cd0 = 0.023728434483470374  # S_wet_to_S *c_friction
+cd0 = 0.023728434483470374  # previously S_wet_to_S *c_friction
 cdi = (cl_cruise**2)/(math.pi*AR*oswald)
 cd_cruise = (4/3)*cd0
 print(cd0)
