430b77439902d3e5308ab1d34eaabb581567eab3
sofia yanes sanchez <85056219+sofia-ys@users.noreply.github.com>
2024-11-13 15:33:43 +0100

    updates

--- matching_diagram.py
+++ matching_diagram.py
@@ -16,7 +16,7 @@ min_speed = (1 / ad.m_fraq_landing * (ad.rho_landing / 2) * ad.speed_stall_lf **
 # W/S [N/m^2] Calculating the maximum wing loading for the landing field length requirement
 landing_field_length = (1 / ad.m_fraq_landing) * (ad.l_fl / ad.c_lfl) * ((ad.rho_lfl_hot * ad.cl_landing) / 2)
 
-cruise_speed = cruise_speed.cruise_thrust_to_weight(wing_loading)
+cruise_speed_value = cruise_speed.cruise_thrust_to_weight(wing_loading)
 
 climb_gradient_119 = climb_gradient.calcGradient119(wing_loading)
 
@@ -28,9 +28,9 @@ climb_gradient_121c = climb_gradient.calcGradient121c(wing_loading)
 
 climb_gradient_121d = climb_gradient.calcGradient121d(wing_loading)
 
-take_off_field_length = take_off_field_length.take_off_field_length(wing_loading)
+take_off_field_length_value = take_off_field_length.take_off_field_length(wing_loading)
 
-climb_rate = climb_rate.climb_rate(wing_loading)
+climb_rate_value = climb_rate.climb_rate(wing_loading)
 
 # Plot size
 plt.figure(figsize=(15, 11))
@@ -41,21 +41,21 @@ plt.ylabel('T/W - [N/N]', fontsize = 15)
 
 plt.axvline(x = min_speed, color = 'royalblue', label = 'Minimum speed')
 plt.axvline(x = landing_field_length, color = 'orangered' , label = 'Landing field length')
-plt.plot(wing_loading, cruise_speed, color = 'indigo', label = 'Cruise speed')
+plt.plot(wing_loading, cruise_speed_value, color = 'indigo', label = 'Cruise speed')
 plt.plot(wing_loading, climb_gradient_119, color = 'red', label = 'Climb gradient 119')
 plt.plot(wing_loading, climb_gradient_121a, color = 'deepskyblue', label = 'Climb gradient 121a')
 plt.plot(wing_loading, climb_gradient_121b, color = 'darkgoldenrod', label = 'Climb gradient 121b')
 plt.plot(wing_loading, climb_gradient_121c, color = 'forestgreen', label = 'Climb gradient 121c')
 plt.plot(wing_loading, climb_gradient_121d, color = 'brown', label = 'Climb gradient 121d')
-plt.plot(wing_loading, take_off_field_length, color = 'darkslategray' , label = 'Take off field length')
-plt.plot(wing_loading, climb_rate, color = 'deeppink', label = 'Climb rate')
-plt.plot(landing_field_length, climb_gradient_121b[int(np.floor(landing_field_length))], marker = 'D', color = 'crimson', markersize = '6', label = 'Design point')
+plt.plot(wing_loading, take_off_field_length_value, color = 'darkslategray' , label = 'Take off field length')
+plt.plot(wing_loading, climb_rate_value, color = 'deeppink', label = 'Climb rate')
+plt.plot(landing_field_length, cruise_speed_value[int(np.floor(landing_field_length))], marker = 'D', color = 'crimson', markersize = '6', label = 'Design point')
 plt.scatter(reference_aircrafts.W / reference_aircrafts.S, reference_aircrafts.T / reference_aircrafts.W, label = 'Reference aircrafts')
 
 plt.xlim(0, 8000)
 plt.ylim(0, 1)
 
 plt.legend(loc = 'upper left')
-print(landing_field_length, climb_gradient_121b[int(np.floor(landing_field_length))])
+print(landing_field_length, cruise_speed_value[int(np.floor(landing_field_length))])
 print(landing_field_length)
 plt.show()
