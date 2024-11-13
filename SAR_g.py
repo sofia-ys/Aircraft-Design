afb1e8b702e1516a1d98f851495c6ad6c8ced1ff
sofia yanes sanchez <85056219+sofia-ys@users.noreply.github.com>
2024-11-13 15:47:30 +0100

    changes

--- SAR_g.py
+++ SAR_g.py
@@ -1,14 +1,14 @@
 import math
-S = 173.77  # wing area m^2
-AR = 9.7# aspect ratio
+S = 131.75  # previously 173.77 wing area m^2
+AR = 9.7  # aspect ratio
 b = math.sqrt(AR*S)  # wing span m  # taper ratio
 
 sweep_quarter = 0.537  # wing sweep rad
-C_D0 = 0.01589727272
-CL= 0.454
+C_D0 = 0.023728434483470374  # previously 0.01589727272
+CL = 0.454
 v_cruise = 256.4704663921364
-B= 11
-C_T= 22*(B**-0.19)*(1*(10**-6))
+B = 11
+C_T = 22*(B**-0.19)*(1*(10**-6))
 
 sweep_quarter = 0.537
 sweep_LE = 0.5871613666551182-0.0053
@@ -28,5 +28,3 @@ SAR= v_cruise/(D_cruise*C_T)
 print(Taper_ratio)
 print(Ctip)
 print(Cr)
-
-
