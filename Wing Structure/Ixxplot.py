e0f56d117b7b4ed1d5222e6c3b4a6b7e9bc438fc
Giorgiamala <giorgiamala23@gmail.com>
2024-11-20 15:25:07 +0100

    hey

--- Wing Structure/Ixxplot.py
+++ Wing Structure/Ixxplot.py
@@ -6,6 +6,12 @@ span_n1 = [18,21,25,30,36,44,51,59,68]
 n1 = [18,21,25,30,36,44,51,59,68]
 span_n2 = [18,21,25,30,36,44,51,59,68]
 n2 = [27,33,38,45,55,65,80,100,120]
+span_t1 = [18,21,25,30,36,44,51,59,68]
+t1 = [18,21,25,30,36,44,51,59,68]
+span_t2 = [18,21,25,30,36,44,51,59,68]
+t2 = [18,21,25,30,36,44,51,59,68]
+An =
+
 
 f = sp.interpolate.interp1d(span_n1,n1,kind="previous",fill_value="extrapolate")
 g = sp.interpolate.interp1d(span_n2,n2,kind="previous",fill_value="extrapolate")
