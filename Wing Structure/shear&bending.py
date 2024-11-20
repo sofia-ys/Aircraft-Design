b30f784e27cf0bafb84c6e4c59321bc79d4c10cd
AdrienEsmiol <esmiol.adrien@gmail.com>
2024-11-20 15:09:28 +0100

    bending code

---
+++ Wing Structure/shear&bending.py
@@ -0,0 +1,18 @@
+import input_xflr_data as func
+import matplotlib.pyplot as plt
+import numpy as np
+
+shear = list(range(32))
+shear[0] = 0
+#yaxis=list(range(32))
+xaxis = [i * 0.59 for i in range(len(shear))]
+
+for i in range(31):
+    L_sec = 0.5*1.225*100*func.get_cl(-17.7097+i*0.59, 0)*func.get_chord(-17.7097+i*0.59, 0)
+    shear[i+1]= shear[i]+ L_sec*0.59
+
+print(shear)
+
+plt.plot(xaxis, shear, marker='o', linestyle='-', color='b')
+plt.grid(True)
+plt.show()
