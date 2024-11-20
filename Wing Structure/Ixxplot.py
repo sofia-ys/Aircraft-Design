3a41a816eee34ba4de07468a60a83af08e07f543
Giorgiamala <giorgiamala23@gmail.com>
2024-11-20 15:20:39 +0100

    hey

--- Wing Structure/Ixxplot.py
+++ Wing Structure/Ixxplot.py
@@ -2,9 +2,10 @@ import math
 import scipy as sp
 from scipy import interpolate
 import Ixx
+span_n1 = [18,21,25,30,36,44,51,59,68]
+n1 = [18,21,25,30,36,44,51,59,68]
+span_n2 = [18,21,25,30,36,44,51,59,68]
+n2 = [27,33,38,45,55,65,80,100,120]
 
-age = [18,21,25,30,36,44,51,59,68]
-pay = [27,33,38,45,55,65,80,100,120]
-
-f = sp.interpolate.interp1d(age,pay,kind="previous",fill_value="extrapolate")
-g = sp.interpolate.interp1d(age,pay,kind="next",fill_value="extrapolate")
+f = sp.interpolate.interp1d(span_n1,n1,kind="previous",fill_value="extrapolate")
+g = sp.interpolate.interp1d(span_n2,n2,kind="previous",fill_value="extrapolate")
