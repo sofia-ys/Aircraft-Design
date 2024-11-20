b7dbf79c57790ce9333eb11f939f8f6a075e3eb1
Goga64 <goga.myak@gmail.com>
2024-11-20 13:46:03 +0100

    abcd

--- Wing Structure/torque.py
+++ Wing Structure/torque.py
@@ -1,7 +1,14 @@
 import math as m
 import scipy as sp
 from scipy import integrate
-
+from extra_constants import *
 thust_torque = 1 #Ignoring the drag becasue invicid fow
 
 
+cm = []
+y = []
+
+S = 1 #Area of that small section of the wing NOT the whole wing
+cm = []  #Moment coefficint of the section
+c = 1 #Chord length at that y position
+M = cm*S*c*q + 1#SOME way to shift the position of the moemnt along the chord  from 0.25c?? to teh flexure axis
