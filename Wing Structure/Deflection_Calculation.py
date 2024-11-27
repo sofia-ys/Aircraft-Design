17fd6fab44b69168ab0e79f6cb950e32033dbee1
mateidragomirae <matei_21@yahoo.com>
2024-11-27 16:47:55 +0100

    test

---
+++ Wing Structure/Deflection_Calculation.py
@@ -0,0 +1,41 @@
+from Ixx import
+import scipy as sp
+import matplotlib.pyplot as plt
+import constants as con
+E = 70 * 10^9 # change it when you choose material
+b = con.b
+#import Moment function
+def bending_moment(y):
+    return 123
+#import moment of inertia function
+def Ixx(y):
+    return 123
+def second_derivative_deflection(y):
+    return -1 * bending_moment(y) / (E * Ixx(y))
+def first_derivative_deflection(y):
+    slope = sp.integrate.quad(second_derivative_deflection,0,y)
+    return slope
+def deflection(y):
+    defl = sp.integrate.quad(first_derivative_deflection,0,y)
+    return defl
+y_tab = []
+step = 0.05
+y = 0
+step_number = 0
+deflection_tab = []
+slope_tab = []
+while y <= b / 2 :
+    y_tab.append(y)
+    y = y + step
+    step_number = step_number + 1
+for el in y_tab:
+    slope_tab.append(first_derivative_deflection(el))
+    deflection_tab.append(deflection(el))
+plt.plot(y_tab, slope_tab, label='Slope')
+plt.xlabel('Position along half-span')
+plt.ylabel('Slope at each point along half-span')
+plt.plot(y_tab, deflection_tab)
+plt.xlabel('Position along half-span')
+plt.ylabel('Deflection')
+
+plt.show()
