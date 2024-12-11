f1c798ad126f66db5c8347d2c1c3266fff6fd4ec
AdamJaneczech <anndyjan@seznam.cz>
2024-12-11 15:15:52 +0100

    constants updated

--- Wing_Box_Detailed/wing_box_constants.py
+++ Wing_Box_Detailed/wing_box_constants.py
@@ -4,10 +4,31 @@ E = 0 * 10**9 # young's modulus [Pa]
 poisson = 0  # poisson's ratio 
 
 # design 1 parameters
-spar_a1 = 0  # spar web length [m]
-spar_b1 = 0  # spar web height [m]
-spar_t1 = 0  # spar web thickness [m]
 k_s1 = [15, 13.6, 12.8, 12.3, 11.9, 11.6, 11.35, 11, 10.8, 10.55, 10.35]  # buckling coefficient for rectangular plate under shear, starting at a/b = 1 and 0.1 increments
-spar_hf1 = 0  # height of front spar [m]
-spar_hr1 = 0  # height of rear spar [m]
 
+# wing box characteristics ALL VALUES FOR ROOT POSITION ONLY
+d_1 = 0.609118 #Place Holder value Length of front spar
+d_2 = 2.582268 #Place holder value for Distance between front and back spar
+d_3 = 0.487393 # Place Holder value for length of back spar
+d_4 = 1.2 # Place Holder  value for distrance between front spar amd multibox spar
+t_1 = 0.005 # Place Holder  value for thickness of spars
+t_2 = 0.005 # Place Holder value for thickness of wing box skin (top and bottom)
+
+
+#Design 1
+span_t1_1 = [0, 3.5, 7]
+t1_1 = [0.007, 0.006, 0.005]
+span_t2_1 = [0, 3.5, 7]
+t2_1 = [0.007, 0.006, 0.005]
+
+#Design 2
+span_t1_2 = [0, 21, 25, 30, 36]
+t1_2 = [0.005, 0.005, 0.005, 0.005, 0.005]
+span_t2_2 = [0, 21, 25, 30, 36]
+t2_2 = [18, 21, 25, 30, 36]
+
+#Design 3
+span_t1_3 = [0, 21, 25, 30, 36]
+t1_3 = [0.005, 0.005, 0.005, 0.005, 0.005]
+span_t2_3 = [0, 21, 25, 30, 36]
+t2_3 = [18, 21, 25, 30, 36]
