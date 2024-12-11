68809afd9989d47e60c03607038956dfc1c7ac67
AdamJaneczech <anndyjan@seznam.cz>
2024-12-11 15:21:52 +0100

    commit

--- Wing_Box_Detailed/shear_buckling.py
+++ Wing_Box_Detailed/shear_buckling.py
@@ -1,5 +1,9 @@
 import numpy as np
 import wing_box_constants as wb
+from Ixx import Wingbox_lengths
+
+def getSparHeight(spar_id, y):
+    d1, d2, d3, d4 = Wingbox_lengths(wb.d_1, wb.d_2, wb.d_3, wb.d_4, )
 
 def criticalShear(k_s, E, poisson, t, b):
     tau_cr = np.pi**2 * k_s * E / (12 * (1 - poisson**2)) * (t/b)**2 
