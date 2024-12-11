8934bf7c49682c2bc688b384a511447339a9c6c8
Giorgiamala <giorgiamala23@gmail.com>
2024-12-11 17:20:39 +0100

    hey

--- Wing Structure/Ixy.py
+++ Wing Structure/Ixy.py
@@ -4,40 +4,27 @@ import math
 from matplotlib import pyplot as plt
 
 import constants as con
+from Ixx import CentroidZcontribution
+from Ixx import Wingbox_lengths
 
-def Wingbox_lengths(d1_root, d2_root, d3_root, d4_root, b, y):
-    taper = con.taper
-    d1_a = d1_root + (d1_root * taper - d1_root) / (b / 2) * y
-    d2_a = d2_root + (d2_root * taper - d2_root) / (b / 2) * y
-    d3_a = d3_root + (d3_root * taper - d3_root) / (b / 2) * y
-    d4_a = d4_root + (d4_root * taper - d4_root) / (b / 2) * y
-    return d1_a, d2_a, d3_a, d4_a
-
-
-def CentroidZcontribution(As, sb, st, alpha, n2, n1, d1, d2, d3, d4, t1, t2):
-    CB_z = sum(As * i * sb * math.sin(alpha) for i in range(0, int(n2)))  #for bottom
-
-    CB_x = sum(As * i * sb * math.cos(alpha) for i in range(0, int(n2)))  #top
-
-    CT_x = sum(As * i * st for i in range(0, int(n1)))  #top
+def Ixx2calculator(d1, d2, L, d3, t1, t2, h, alpha, n1, n2, As, sb, d4):
     u = d4 * math.tan(alpha)
-    if d4 > 0:
-        total_area = ((t1 * d1 + d2 * t2 + t1 * d3) + (d2 * t2 / math.cos(alpha))) + (As * (n1 + n2))
-        h = ((d1 * d2 * t2) + (t1 * (d1 ** 2) / 2) + (t1 * d3 * d1) - ((d3 ** 2) * t1 / 2) + (
-                    (d2 ** 2) * t2 * math.tan(alpha) / (
-                        2 * math.cos(alpha))) + CB_z + d1 * n1 * As) / total_area  #Z centroid positon single box
-        x = (((d2 ** 2) * t2 / 2) + d3 * d2 * t1 + (
-                    (d2 ** 2) * t2 / (2 * math.cos(alpha)) + CB_x + CT_x))/total_area  #X centroid position single box
-    else:
-        total_area = ((t1 * d1 + d2 * t2 + t1 * d3) + (d2 * t2 / math.cos(alpha))) + (As * (n1 + n2)) + (t1 * (d1 - u))
-        h = ((d1 * d2 * t2) + (t1 * (d1 ** 2) / 2) + (t1 * d3 * d1) - ((d3 ** 2) * t1 / 2) + (
-                    (d2 ** 2) * t2 * math.tan(alpha) / (2 * math.cos(alpha))) + CB_z + d1 * n1 * As + (
-                         u + ((d1 - u) / 2) * (t1 * (d1 - u)))) / total_area  #Z centroid positon multi box
-        x = (((d2 ** 2) * t2 / 2) + d3 * d2 * t1 + ((d2 ** 2) * t2 / (2 * math.cos(alpha)) + CB_x + CT_x) + (
-                    (t1 * (d1 - u)) * d4))/total_area  #X centroid positon multi box
-    return h, x
+    I1 = 1 / 12 * d1 ** 3 * t1 + d1 * t1 * (d1 / 2 - h) ** 2
+    I2 = 1 / 12 * L ** 3 * t1 + L * t1 * (d2 * math.sin(alpha) + d3 / 2 - h) ** 2
+    I3 = (1 / 12 * L ** 3 * t2 * (math.sin(alpha)) ** 2)+L*t2*(h-d1/2*math.sin(alpha))**2
+    I4 = t2 * d2 * (d1 - h) ** 2
+    I5 = As * n1 * (d1 - h) ** 2
+    I6 = 0
+    I7 = 1 / 12 * (d1 - u) ** 3 * t1 + t1 * (d1 - u) * (d1 / 2 - h + u / 2) ** 2
+    for i in range(0, int(n2)):
+        Ii = As * (h - i * math.sin(alpha) * sb) ** 2
+        I6 += Ii
+    I = I1 + I2 + I3 + I4 + I5 + I6 + I7
 
-def Ixxfinal(design_choice, y):
+    return I
+
+
+def Ixyfinal(design_choice, y):
     d1_root = con.d_1
     d2_root = con.d_2
     d3_root = con.d_3
@@ -103,3 +90,26 @@ def Ixxfinal(design_choice, y):
     else:
         Ixx = Ixxcalculator(d1, d2, L, d3, t1, t2, h, alpha, n1, n2, As, sb)
     return Ixx
+
+
+y_tab = []
+Ixx_tab=[]
+i = 0
+step = 0.05
+count = 0
+b = con.b
+
+while i <= b / 2:
+    i = i + step
+    y_tab.append(i)
+    count = count + 1
+
+for y in y_tab:
+     Ixy = Ixyfinal(1, y)
+     Ixy_tab.append(Ixx)
+
+plt.plot(y_tab, Ixx_tab)
+plt.xlabel('Position along half-span (m)')
+plt.ylabel('Ixx (m^4)')
+plt.title('Moment of inertia at each position along the half-span ')
+plt.show()
