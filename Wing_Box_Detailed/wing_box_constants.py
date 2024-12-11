facebae41c2d46a151d9974c0d79294fd76b7ed4
AdamJaneczech <anndyjan@seznam.cz>
2024-12-11 15:22:37 +0100

    commit

--- Wing_Box_Detailed/wing_box_constants.py
+++ Wing_Box_Detailed/wing_box_constants.py
@@ -3,7 +3,6 @@
 E = 0 * 10**9 # young's modulus [Pa]
 poisson = 0  # poisson's ratio 
 
-# design 1 parameters
 k_s1 = [15, 13.6, 12.8, 12.3, 11.9, 11.6, 11.35, 11, 10.8, 10.55, 10.35]  # buckling coefficient for rectangular plate under shear, starting at a/b = 1 and 0.1 increments
 
 # wing box characteristics ALL VALUES FOR ROOT POSITION ONLY
@@ -16,18 +15,21 @@ t_2 = 0.005 # Place Holder value for thickness of wing box skin (top and bottom)
 
 
 #Design 1
+k_s1 = [15, 13.6, 12.8, 12.3, 11.9, 11.6, 11.35, 11, 10.8, 10.55, 10.35]  # buckling coefficient for rectangular plate under shear, starting at a/b = 1 and 0.1 increments
 span_t1_1 = [0, 3.5, 7]
 t1_1 = [0.007, 0.006, 0.005]
 span_t2_1 = [0, 3.5, 7]
 t2_1 = [0.007, 0.006, 0.005]
 
 #Design 2
+k_s2 = [15, 13.6, 12.8, 12.3, 11.9, 11.6, 11.35, 11, 10.8, 10.55, 10.35]  # buckling coefficient for rectangular plate under shear, starting at a/b = 1 and 0.1 increments
 span_t1_2 = [0, 21, 25, 30, 36]
 t1_2 = [0.005, 0.005, 0.005, 0.005, 0.005]
 span_t2_2 = [0, 21, 25, 30, 36]
 t2_2 = [18, 21, 25, 30, 36]
 
 #Design 3
+k_s3 = [15, 13.6, 12.8, 12.3, 11.9, 11.6, 11.35, 11, 10.8, 10.55, 10.35]  # buckling coefficient for rectangular plate under shear, starting at a/b = 1 and 0.1 increments
 span_t1_3 = [0, 21, 25, 30, 36]
 t1_3 = [0.005, 0.005, 0.005, 0.005, 0.005]
 span_t2_3 = [0, 21, 25, 30, 36]
