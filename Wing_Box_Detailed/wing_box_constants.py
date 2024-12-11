4a389097fb3ee73a149f49049d7e108e4c62519e
sofia yanes sanchez <85056219+sofia-ys@users.noreply.github.com>
2024-12-11 16:42:24 +0100

    properties

--- Wing_Box_Detailed/wing_box_constants.py
+++ Wing_Box_Detailed/wing_box_constants.py
@@ -1,7 +1,7 @@
 ''' constants for the final wing box design '''
 # material properties
-E = 0 * 10**9 # young's modulus [Pa]
-poisson = 0  # poisson's ratio 
+E = 72.4 * 10**9 # young's modulus [Pa]
+poisson = 0.33  # poisson's ratio 
 
 k_s1 = [15, 13.6, 12.8, 12.3, 11.9, 11.6, 11.35, 11, 10.8, 10.55, 10.35]  # buckling coefficient for rectangular plate under shear, starting at a/b = 1 and 0.1 increments
 b = 35.48
