9e09741fd569434e5ecc7fa4ad9a6efb4816afb2
Goga64 <goga.myak@gmail.com>
2024-12-11 15:22:56 +0100

    a

--- Wing_Box_Detailed/column buckling.py
+++ Wing_Box_Detailed/column buckling.py
@@ -8,10 +8,7 @@ E = 72.4 * 10**9
 A = 0
 
 
-def moments_of_inetria_L_stringer(): # The reference x coordinate is the left side of the L and the reference y coordinate is the bottom of the L
-    height = 83
-    width = 53.3
-    thickness = 3
+def moments_of_inetria_L_stringer(height, width, thickness): # The reference x coordinate is the left side of the L and the reference y coordinate is the bottom of the L
     y = ((((height - thickness)/2 + thickness)*(height - thickness)*thickness + thickness / 2 * width * thickness)
          / ((height - thickness)*thickness + width * thickness))
     x = ((thickness / 2 * (height - thickness) * thickness + width / 2 * width * thickness)
@@ -20,11 +17,11 @@ def moments_of_inetria_L_stringer(): # The reference x coordinate is the left si
             (height / 2 - y) ** 2 * height * thickness + (thickness / 2 - y) ** 2 * (width - thickness) * thickness)
     return I_xx
 
-print(moments_of_inetria_L_stringer())
 
-def calc_crit_o(K,E,I,A,L):
-    E = E*10**(-12)
+
+def calc_crit_sigma(K,E,I,A,L):
+    I = I*10**(-12)
     A = A*10^(-6)
-    return (K*m.pi*E*I)/(A*L**2)
+    return (K * m.pi**(2) * E * I)/(A * L**2)
 
-print("Tall = ", calc_crit_o(0, E, 125532, 300, ))
+print("Tall = ", calc_crit_sigma(1, 72.4 * 10**9, moments_of_inetria_L_stringer(83,53.3,3), 300, 10))
