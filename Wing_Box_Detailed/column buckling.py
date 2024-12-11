d2d0ece47e34dc1f398ce2260bae0465cd1fc143
Goga64 <goga.myak@gmail.com>
2024-12-11 15:38:39 +0100

    Fisnished column buckling

--- Wing_Box_Detailed/column buckling.py
+++ Wing_Box_Detailed/column buckling.py
@@ -25,4 +25,5 @@ def calc_crit_sigma(K,E,I,A,L):
     A = A*10**(-6)
     return (K * m.pi**(2) * E * I)/(A * L**2)
 
-print("Tall = ", calc_crit_sigma(1, 72.4 * 10**9, moments_of_inetria_L_stringer(83,53.3,3), A(83,53.3,3), 10))
+print("Design 2 =", round(calc_crit_sigma(1, 72.4 * 10**9, moments_of_inetria_L_stringer(83,53.3,3), A(83,53.3,3), 10)/1000000,2), "MPa") # 10m is the shortest stinger
+print("Design 1,3 =", round(calc_crit_sigma(1, 72.4 * 10**9, moments_of_inetria_L_stringer(63,40.3,3), A(63,40.3,3), 7)/1000000,2), "MPa") # 7m is the shortest stringer
