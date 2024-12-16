a8d7c44a54e327e9542c78d8f737e25639f026bc
Giorgiamala <giorgiamala23@gmail.com>
2024-12-16 16:24:57 +0100

    hey

--- Wing Structure/bendingstress.py
+++ Wing Structure/bendingstress.py
@@ -1,4 +1,7 @@
 import math as math
+
+
+def findlongestdistance(d1, d2, L, d3, t1, t2, h, alpha, n1, n2, As, sb, x, st)
 f1= math.sqrt(x**2 + h**2)
 f2= math.sqrt(x**2 + (d1-h)**2)
 f3= math.sqrt((d2-x)**2 + (d1-h)**2)
