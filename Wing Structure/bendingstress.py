173f15f1853042aeb1300bc8a59d6cd4861a6925
MaximeEveraert-Pham <maxime.everaert.pham@gmail.com>
2024-12-16 16:24:39 +0100

    Update bendingstress.py

--- Wing Structure/bendingstress.py
+++ Wing Structure/bendingstress.py
@@ -3,3 +3,9 @@ f1= math.sqrt(x**2 + h**2)
 f2= math.sqrt(x**2 + (d1-h)**2)
 f3= math.sqrt((d2-x)**2 + (d1-h)**2)
 f4 = math.sqrt((d2-x)**2 + (d1-d3-h)**2)
+
+
+e1= x
+e2 = x
+e3 = d2 -x
+e4 = d2 - x
