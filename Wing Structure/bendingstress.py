90ac9f67b729281ab71c98772c7d2054f5ee3adc
Giorgiamala <giorgiamala23@gmail.com>
2024-12-18 15:57:10 +0100

    hey

--- Wing Structure/bendingstress.py
+++ Wing Structure/bendingstress.py
@@ -5,32 +5,31 @@ from Izz import Izzcalculator
 
 
 def findlongestdistance(d1, d2, L, d3, d4, t1, t2, h, alpha, n1, n2, As, x, design_choice, y):
-    f1 = math.sqrt(x**2 + h**2)
-    f2 = math.sqrt(x**2 + (d1-h)**2)
-    f3 = math.sqrt((d2-x)**2 + (d1-h)**2)
-    f4 = math.sqrt((d2-x)**2 + (d1-d3-h)**2)
+    f1 = math.sqrt(x ** 2 + h ** 2)
+    f2 = math.sqrt(x ** 2 + (d1 - h) ** 2)
+    f3 = math.sqrt((d2 - x) ** 2 + (d1 - h) ** 2)
+    f4 = math.sqrt((d2 - x) ** 2 + (d1 - d3 - h) ** 2)
     e1 = x
     e2 = x
     e3 = d2 - x
     e4 = d2 - x
-    P1 = math.acos(e1/f1)
-    P2 = math.acos(e2/f2)
-    P3 = math.acos(e3/f3)
-    P4 = math.acos(e4/f4)
+    P1 = math.acos(e1 / f1)
+    P2 = math.acos(e2 / f2)
+    P3 = math.acos(e3 / f3)
+    P4 = math.acos(e4 / f4)
     Ixz = Ixzfinal(design_choice, y)
     Izz = Izzcalculator(d1, d2, d3, d4, alpha, t1, t2, x, As, n1, n2, L, y)
-    A = math.atan(Izz/Ixz)
+    A = math.atan(Izz / Ixz)
     B1 = P1 + A
     B2 = P2 - A
     B3 = P3 + A
     B4 = P4 - A
-    g1 = f1 * math.sin(B1) #distance from neutral axis for corner 1
+    g1 = f1 * math.sin(B1)  # distance from neutral axis for corner 1
     g2 = f2 * math.sin(B2)
     g3 = f3 * math.sin(B3)
     g4 = f3 * math.sin(B4)
 
     values = [g1, g2, g3, g4]
-    labels = [1, 2, 3, 4]
 
     max_value = max(values)
     max_index = values.index(max_value)
@@ -49,4 +48,3 @@ def findlongestdistance(d1, d2, L, d3, d4, t1, t2, h, alpha, n1, n2, As, x, desi
         c2 = h
 
     return c1, c2
-
