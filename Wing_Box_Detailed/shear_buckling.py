b1237ecb34501a7de8384cb172084efca27c0964
AdamJaneczech <anndyjan@seznam.cz>
2024-12-18 16:51:31 +0100

    critical shears working

--- Wing_Box_Detailed/shear_buckling.py
+++ Wing_Box_Detailed/shear_buckling.py
@@ -4,6 +4,7 @@ import sys
 sys.path.append('Wing Structure')
 import Ixx
 import matplotlib.pyplot as plt
+import math
 #half-span divided into 100 sections
 y_values = array = np.linspace(0, wb.b / 2, 100)
 
@@ -11,11 +12,11 @@ def getSparHeight(design_id, spar_id, y): # get b
     d = []
     d = Ixx.Wingbox_lengths(wb.all_spar_d[design_id][0], wb.all_spar_d[design_id][1], wb.all_spar_d[design_id][2], wb.all_spar_d[design_id][3], wb.b, y)
     if(spar_id == 1):
-        return d[0]
+        return d[0] #dimension d1 for first spar height
     elif(spar_id == 2):
-        return d[2]
+        return d[2] #dimension d3 for the second spar height
     elif(spar_id == 3):
-        return d[3]
+        return (d[0] - d[3]/d[1] * (d[0] - d[2])) #d4 for middle spar, if applicable: this calculates the height of the middle spar
     else:
         print("error")
 
@@ -24,7 +25,7 @@ def getSparThickness(span_t1, t1, y):
     for i in range(len(span_t1) - 1):  # adjustable to the number of discontinuities for the design types
         if span_t1[i] <= y and y <= span_t1[i+1]:  # between the first point in the list and the next point
             spar_thickness = t1[i]  # spar thickness is that point
-        elif wb.b/2 > y and y > span_t1[i+1]:
+        elif y >= span_t1[i+1]:
             spar_thickness = t1[i+1]
         else:
             break
@@ -100,11 +101,11 @@ t2_spans = [wb.span_t2_1, wb.span_t2_2, wb.span_t2_3]
 q_values = [wb.q1, wb.q2, wb.q3]
 
 def plotCritShear(E, poisson, y_values):
-    for design_id in range(0,3):
+    for design_id in range(0,3): #design_id is from 0 to 2, represents design cases 1 to 3!
         for y in y_values:
             crit_shear_spar1[design_id].append(getCritShear(get_ks(get_bay_width(y, wb.ribs), getSparHeight(design_id, 1, y)), E, poisson, getSparThickness(t1_spans[design_id], t1_vals[design_id], y), getSparHeight(design_id, 1, y)))
             crit_shear_spar2[design_id].append(getCritShear(get_ks(get_bay_width(y, wb.ribs), getSparHeight(design_id, 2, y)), E, poisson, getSparThickness(t1_spans[design_id], t1_vals[design_id], y), getSparHeight(design_id, 2, y)))      
-            if(y < q_values[design_id]):
+            if(design_id < 2):
                 crit_shear_spar3[design_id].append(getCritShear(get_ks(get_bay_width(y, wb.ribs), getSparHeight(design_id, 3, y)), E, poisson, getSparThickness(t1_spans[design_id], t1_vals[design_id], y), getSparHeight(design_id, 3, y)))
             else:
                 crit_shear_spar3[design_id].append(0)
@@ -116,7 +117,12 @@ def plotCritShear(E, poisson, y_values):
     for designCase in range(0,3):
         plt.plot(y_values, critShearSpar1[designCase], label='Critical shear for spar 1, DC ' + str(designCase + 1))
         plt.plot(y_values, critShearSpar2[designCase], label='Critical shear for spar 2, DC ' + str(designCase + 1))
-        plt.plot(y_values, critShearSpar3[designCase], label='Critical shear for spar 3, DC ' + str(designCase + 1))
+        print("Minimum critical shear [Pa] for spar 1 (d1), design case " + str(designCase + 1) + ": " + str(np.min(critShearSpar1[designCase])))
+        print("Minimum critical shear [Pa] for spar 2 (d3), design case " + str(designCase + 1) + ": " + str(np.min(critShearSpar2[designCase])))
+        if(designCase < 2):
+            plt.plot(y_values, critShearSpar3[designCase], label='Critical shear for spar 3, DC ' + str(designCase + 1))
+            print("Minimum critical shear [Pa] for spar 3 (mid spar), design case " + str(designCase + 1) + ": " + str(np.min(critShearSpar3[designCase])))
+        
     plt.xlabel('Spanwise Position (m)')
     plt.ylabel('Critical shear stress (Pa)')
     plt.title('Critical shear stress along the wing span')
